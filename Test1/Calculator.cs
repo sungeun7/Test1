5fa0ff9331d1daf947530e7bcbd6a2d532742e72
dokki <dokki@LAPTOP-HK12KLH9>
2022-12-12 15:33:44 +0900

    초기 커밋.

---
+++ Test1/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Calculator
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+
+        internal static double DoOperation(double cleanNum1, double cleanNum2, string op)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
